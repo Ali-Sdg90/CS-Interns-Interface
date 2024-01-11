8630caee5ab9821763f695b1f2065d36c45adb3b
Ali Sadeghi <ali909392@gmail.com>
2024-01-11 20:50:42 +0330

    Add classes

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,168 @@
 "use strict";
+class Intern {
+    fullName;
+    course;
+    animal;
+    coordinators;
+    commitment;
+    generalInfo;
+    contactInfo;
+    socialLinks;
+    constructor(fullName, course, animal, coordinators, commitment, generalInfo, contactInfo, socialLinks) {
+        this.fullName = fullName;
+        this.course = course;
+        this.animal = animal;
+        this.coordinators = coordinators;
+        this.commitment = commitment;
+        this.generalInfo = generalInfo;
+        this.contactInfo = contactInfo;
+        this.socialLinks = socialLinks;
+    }
+    currentStep() {
+        return 0;
+    }
+}
+class Commitment {
+    text;
+    date;
+    constructor(text, date) {
+        this.text = text;
+        this.date = date;
+    }
+}
+class GeneralInfo {
+    city;
+    university;
+    fieldOfStudy;
+    experiences;
+    constructor(city, university, fieldOfStudy, experiences) {
+        this.city = city;
+        this.university = university;
+        this.fieldOfStudy = fieldOfStudy;
+        this.experiences = experiences;
+    }
+}
+class ContactInfo {
+    phone;
+    email;
+    telegram;
+    skype;
+    constructor(phone, email, telegram, skype) {
+        this.phone = phone;
+        this.email = email;
+        this.telegram = telegram;
+        this.skype = skype;
+    }
+}
+class SocialLinks {
+    linkedin;
+    twitter;
+    github;
+    jsfiddle;
+    stackoverflow;
+    repl;
+    typingClub;
+    constructor(linkedin, twitter, github, jsfiddle, stackoverflow, repl, typingClub) {
+        this.linkedin = linkedin;
+        this.twitter = twitter;
+        this.github = github;
+        this.jsfiddle = jsfiddle;
+        this.stackoverflow = stackoverflow;
+        this.repl = repl;
+        this.typingClub = typingClub;
+    }
+}
+class Course {
+    courseTitle;
+    steps;
+    progress;
+    startDate;
+    endDate;
+    constructor(courseTitle, steps, progress, startDate, endDate) {
+        this.courseTitle = courseTitle;
+        this.steps = steps;
+        this.progress = progress;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
+class Step {
+    startDate;
+    promises;
+    completeDate;
+    studyItems;
+    tasks;
+    mentoringCourse;
+    youtubeLink;
+    coordinatorNote;
+    technicalMentorsNote;
+    mentorsNote;
+    constructor(startDate, promises, completeDate, studyItems, tasks, mentoringCourse, youtubeLink, coordinatorNote, technicalMentorsNote, mentorsNote) {
+        this.startDate = startDate;
+        this.promises = promises;
+        this.completeDate = completeDate;
+        this.studyItems = studyItems;
+        this.tasks = tasks;
+        this.mentoringCourse = mentoringCourse;
+        this.youtubeLink = youtubeLink;
+        this.coordinatorNote = coordinatorNote;
+        this.technicalMentorsNote = technicalMentorsNote;
+        this.mentorsNote = mentorsNote;
+    }
+    isDone() {
+        return true;
+    }
+}
+class StudyItem {
+    title;
+    telegramPost;
+    tweeterPost;
+    linkedinPost;
+    constructor(title, telegramPost, tweeterPost, linkedinPost) {
+        this.title = title;
+        this.telegramPost = telegramPost;
+        this.tweeterPost = tweeterPost;
+        this.linkedinPost = linkedinPost;
+    }
+    isDone() {
+        return true;
+    }
+}
+class Task {
+    title;
+    telegramPost;
+    tweeterPost;
+    linkedinPost;
+    jsfiddleLink;
+    githubLink;
+    constructor(title, telegramPost, tweeterPost, linkedinPost, jsfiddleLink, githubLink) {
+        this.title = title;
+        this.telegramPost = telegramPost;
+        this.tweeterPost = tweeterPost;
+        this.linkedinPost = linkedinPost;
+        this.jsfiddleLink = jsfiddleLink;
+        this.githubLink = githubLink;
+    }
+    isDone() {
+        return true;
+    }
+}
+class MentoringCourse {
+    courseTitle;
+    videoLinks;
+    videoReportLinks;
+    notes;
+    constructor(courseTitle, videoLinks, videoReportLinks, notes) {
+        this.courseTitle = courseTitle;
+        this.videoLinks = videoLinks;
+        this.videoReportLinks = videoReportLinks;
+        this.notes = notes;
+    }
+    progress() {
+        return 0;
+    }
+    isDone() {
+        return true;
+    }
+}
 //# sourceMappingURL=index.js.map
--- TS/index.ts
+++ TS/index.ts
@@ -1,35 +1,35 @@
-interface Intern {
+interface InternInterface {
     fullName: string;
-    course: Course;
+    course: CourseInterface;
     animal: string;
     currentStep: () => number;
     coordinators: string[];
-    commitment: Commitment;
-    generalInfo: GeneralInfo;
-    contactInfo: ContactInfo;
-    socialLinks: SocialLinks;
+    commitment: CommitmentInterface;
+    generalInfo: GeneralInfoInterface;
+    contactInfo: ContactInfoInterface;
+    socialLinks: SocialLinksInterface;
 }
 
-interface Commitment {
+interface CommitmentInterface {
     text: string;
     date: Date;
 }
 
-interface GeneralInfo {
+interface GeneralInfoInterface {
     city: string;
     university: string;
     fieldOfStudy: string;
     experiences: string;
 }
 
-interface ContactInfo {
+interface ContactInfoInterface {
     phone: string;
     email: string;
     telegram: string;
     skype: string;
 }
 
-interface SocialLinks {
+interface SocialLinksInterface {
     linkedin: string;
     twitter: string;
     github: string;
@@ -39,21 +39,21 @@ interface SocialLinks {
     typingClub: string;
 }
 
-interface Course {
+interface CourseInterface {
     courseTitle: "Web" | "Machine Learning" | "C#";
-    steps: Step[];
+    steps: StepInterface[];
     progress: number;
     startDate: Date;
     endDate: Date;
 }
 
-interface Step {
+interface StepInterface {
     startDate: Date;
     promises: Date[];
     completeDate: Date;
-    studyItems: StudyItem[];
-    tasks: Task[];
-    mentoringCourse: MentoringCourse;
+    studyItems: StudyItemInterface[];
+    tasks: TaskInterface[];
+    mentoringCourse: MentoringCourseInterface;
     youtubeLink: string;
     coordinatorNote: string[];
     technicalMentorsNote: string[];
@@ -61,7 +61,7 @@ interface Step {
     isDone: () => boolean;
 }
 
-interface StudyItem {
+interface StudyItemInterface {
     title: string;
     telegramPost: [string, Date][];
     tweeterPost: [string, Date][];
@@ -69,7 +69,7 @@ interface StudyItem {
     isDone: () => boolean;
 }
 
-interface Task {
+interface TaskInterface {
     title: string;
     telegramPost: [string, Date][];
     tweeterPost: [string, Date][];
@@ -79,11 +79,261 @@ interface Task {
     isDone: () => boolean;
 }
 
-interface MentoringCourse {
+interface MentoringCourseInterface {
     courseTitle: "Communication" | "Couching";
     videoLinks: string[];
     videoReportLinks: string[];
     progress: () => number;
     isDone: () => boolean;
     notes: string[];
+}
+
+// Classes:
+
+class Intern implements InternInterface {
+    fullName: string;
+    course: CourseInterface;
+    animal: string;
+    coordinators: string[];
+    commitment: CommitmentInterface;
+    generalInfo: GeneralInfoInterface;
+    contactInfo: ContactInfoInterface;
+    socialLinks: SocialLinksInterface;
+
+    constructor(
+        fullName: string,
+        course: CourseInterface,
+        animal: string,
+        coordinators: string[],
+        commitment: CommitmentInterface,
+        generalInfo: GeneralInfoInterface,
+        contactInfo: ContactInfoInterface,
+        socialLinks: SocialLinksInterface
+    ) {
+        this.fullName = fullName;
+        this.course = course;
+        this.animal = animal;
+        this.coordinators = coordinators;
+        this.commitment = commitment;
+        this.generalInfo = generalInfo;
+        this.contactInfo = contactInfo;
+        this.socialLinks = socialLinks;
+    }
+
+    currentStep(): number {
+        return 0;
+    }
+}
+
+class Commitment implements CommitmentInterface {
+    text: string;
+    date: Date;
+
+    constructor(text: string, date: Date) {
+        this.text = text;
+        this.date = date;
+    }
+}
+
+class GeneralInfo implements GeneralInfoInterface {
+    city: string;
+    university: string;
+    fieldOfStudy: string;
+    experiences: string;
+
+    constructor(
+        city: string,
+        university: string,
+        fieldOfStudy: string,
+        experiences: string
+    ) {
+        this.city = city;
+        this.university = university;
+        this.fieldOfStudy = fieldOfStudy;
+        this.experiences = experiences;
+    }
+}
+
+class ContactInfo implements ContactInfoInterface {
+    phone: string;
+    email: string;
+    telegram: string;
+    skype: string;
+
+    constructor(phone: string, email: string, telegram: string, skype: string) {
+        this.phone = phone;
+        this.email = email;
+        this.telegram = telegram;
+        this.skype = skype;
+    }
+}
+
+class SocialLinks implements SocialLinksInterface {
+    linkedin: string;
+    twitter: string;
+    github: string;
+    jsfiddle: string;
+    stackoverflow: string;
+    repl: string;
+    typingClub: string;
+
+    constructor(
+        linkedin: string,
+        twitter: string,
+        github: string,
+        jsfiddle: string,
+        stackoverflow: string,
+        repl: string,
+        typingClub: string
+    ) {
+        this.linkedin = linkedin;
+        this.twitter = twitter;
+        this.github = github;
+        this.jsfiddle = jsfiddle;
+        this.stackoverflow = stackoverflow;
+        this.repl = repl;
+        this.typingClub = typingClub;
+    }
+}
+
+class Course implements CourseInterface {
+    courseTitle: "Web" | "Machine Learning" | "C#";
+    steps: StepInterface[];
+    progress: number;
+    startDate: Date;
+    endDate: Date;
+
+    constructor(
+        courseTitle: "Web" | "Machine Learning" | "C#",
+        steps: StepInterface[],
+        progress: number,
+        startDate: Date,
+        endDate: Date
+    ) {
+        this.courseTitle = courseTitle;
+        this.steps = steps;
+        this.progress = progress;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
+
+class Step implements StepInterface {
+    startDate: Date;
+    promises: Date[];
+    completeDate: Date;
+    studyItems: StudyItemInterface[];
+    tasks: TaskInterface[];
+    mentoringCourse: MentoringCourseInterface;
+    youtubeLink: string;
+    coordinatorNote: string[];
+    technicalMentorsNote: string[];
+    mentorsNote: string[];
+
+    constructor(
+        startDate: Date,
+        promises: Date[],
+        completeDate: Date,
+        studyItems: StudyItemInterface[],
+        tasks: TaskInterface[],
+        mentoringCourse: MentoringCourseInterface,
+        youtubeLink: string,
+        coordinatorNote: string[],
+        technicalMentorsNote: string[],
+        mentorsNote: string[]
+    ) {
+        this.startDate = startDate;
+        this.promises = promises;
+        this.completeDate = completeDate;
+        this.studyItems = studyItems;
+        this.tasks = tasks;
+        this.mentoringCourse = mentoringCourse;
+        this.youtubeLink = youtubeLink;
+        this.coordinatorNote = coordinatorNote;
+        this.technicalMentorsNote = technicalMentorsNote;
+        this.mentorsNote = mentorsNote;
+    }
+
+    isDone(): boolean {
+        return true;
+    }
+}
+
+class StudyItem implements StudyItemInterface {
+    title: string;
+    telegramPost: [string, Date][];
+    tweeterPost: [string, Date][];
+    linkedinPost: [string, Date][];
+
+    constructor(
+        title: string,
+        telegramPost: [string, Date][],
+        tweeterPost: [string, Date][],
+        linkedinPost: [string, Date][]
+    ) {
+        this.title = title;
+        this.telegramPost = telegramPost;
+        this.tweeterPost = tweeterPost;
+        this.linkedinPost = linkedinPost;
+    }
+
+    isDone(): boolean {
+        return true;
+    }
+}
+
+class Task implements TaskInterface {
+    title: string;
+    telegramPost: [string, Date][];
+    tweeterPost: [string, Date][];
+    linkedinPost: [string, Date][];
+    jsfiddleLink?: string;
+    githubLink?: string;
+
+    constructor(
+        title: string,
+        telegramPost: [string, Date][],
+        tweeterPost: [string, Date][],
+        linkedinPost: [string, Date][],
+        jsfiddleLink?: string,
+        githubLink?: string
+    ) {
+        this.title = title;
+        this.telegramPost = telegramPost;
+        this.tweeterPost = tweeterPost;
+        this.linkedinPost = linkedinPost;
+        this.jsfiddleLink = jsfiddleLink;
+        this.githubLink = githubLink;
+    }
+
+    isDone(): boolean {
+        return true;
+    }
+}
+
+class MentoringCourse implements MentoringCourseInterface {
+    courseTitle: "Communication" | "Couching";
+    videoLinks: string[];
+    videoReportLinks: string[];
+    notes: string[];
+
+    constructor(
+        courseTitle: "Communication" | "Couching",
+        videoLinks: string[],
+        videoReportLinks: string[],
+        notes: string[]
+    ) {
+        this.courseTitle = courseTitle;
+        this.videoLinks = videoLinks;
+        this.videoReportLinks = videoReportLinks;
+        this.notes = notes;
+    }
+
+    progress(): number {
+        return 0;
+    }
+
+    isDone(): boolean {
+        return true;
+    }
 }
